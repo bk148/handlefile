e942ca9f552996c16387aad178ba20a211811b60
b148 <blaise.katende@outlook.com>
2025-03-18 22:46:37 +0100

    rich and logger maj  V4

--- model/model_v4.py
+++ model/model_v4.py
@@ -1,10 +1,12 @@
+import concurrent
+import time
 import os
 import requests
 import logging
 from urllib.parse import quote
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from rich.progress import Progress, TextColumn, BarColumn, TimeRemainingColumn
-from utilis.logger import MigrationLogger
+from concurrent.futures import ThreadPoolExecutor
+from rich.progress import Progress, TextColumn, BarColumn
+from utilis.logger import MigrationLogger  # Importer le MigrationLogger
 
 
 class ModelGraphTransfer:
@@ -76,6 +78,10 @@ class ModelGraphTransfer:
 
     def upload_file_to_channel(self, site_id, parent_item_id, file_path):
         file_name = os.path.basename(file_path)
+        file_size = os.path.getsize(file_path)
+
+        if file_size >= 1 * 1024 * 1024 * 1024:  # 1 Go
+            return self.upload_large_files(site_id, parent_item_id, file_path)
 
         if self.item_exists(site_id, parent_item_id, file_name):
             self.logger.log_info(f"Le fichier '{file_name}' existe déjà, il est ignoré.")
@@ -105,45 +111,23 @@ class ModelGraphTransfer:
             response.raise_for_status()
             upload_url = response.json().get('uploadUrl')
 
-            # Taille du fichier et taille des chunks
-            file_size = os.path.getsize(file_path)
-            chunk_size = 50 * 1024 * 1024  # 50 MB
-            buffer_size = 10 * 1024 * 1024  # 10 MB
-
-            # Fonction pour uploader un chunk
-            def upload_chunk(start, end, chunk_data):
-                chunk_headers = {
-                    'Content-Length': str(len(chunk_data)),
-                    'Content-Range': f'bytes {start}-{end - 1}/{file_size}'
-                }
-                for _ in range(3):  # 3 tentatives en cas d'échec
-                    try:
-                        response = requests.put(upload_url, headers=chunk_headers, data=chunk_data,
-                                              proxies=self.proxies)
-                        response.raise_for_status()
-                        return True
-                    except requests.exceptions.RequestException as e:
-                        self.logger.log_warning(f"Tentative échouée pour le chunk {start}-{end} : {e}")
-                return False
-
-            # Lire et uploader les chunks en parallèle
-            with ThreadPoolExecutor(max_workers=4) as executor:  # 4 threads pour l'upload parallèle
-                futures = []
-                with open(file_path, 'rb', buffering=buffer_size) as file:
-                    for start in range(0, file_size, chunk_size):
-                        end = min(start + chunk_size, file_size)
-                        file.seek(start)
-                        chunk_data = file.read(end - start)
-                        futures.append(executor.submit(upload_chunk, start, end, chunk_data))
-
-                # Vérifier les résultats des uploads
-                for future in as_completed(futures):
-                    if not future.result():
-                        raise Exception("Échec de l'upload d'un chunk après plusieurs tentatives.")
+            # Upload du fichier par chunks
+            chunk_size = 20 * 1024 * 1024  # 20 MB
+            with open(file_path, 'rb') as file:
+                file_size = os.path.getsize(file_path)
+                for i in range(0, file_size, chunk_size):
+                    chunk_data = file.read(chunk_size)
+                    chunk_headers = {
+                        'Content-Length': str(len(chunk_data)),
+                        'Content-Range': f'bytes {i}-{i + len(chunk_data) - 1}/{file_size}'
+                    }
+                    chunk_response = requests.put(upload_url, headers=chunk_headers, data=chunk_data,
+                                                  proxies=self.proxies)
+                    chunk_response.raise_for_status()
 
             self.logger.log_info(f"Fichier volumineux '{file_name}' uploadé avec succès.")
             return file_name, "uploaded"
-        except Exception as e:
+        except requests.exceptions.RequestException as e:
             error_message = f"Erreur lors de l'upload du fichier volumineux '{file_name}' : {e}"
             self.logger.log_error(error_message)
             self.error_logs["File Error"].append(
@@ -159,7 +143,8 @@ class ModelGraphTransfer:
             drive_id = files_folder_response['parentReference']['driveId']
             parent_item_id = files_folder_response['id']
         else:
-            logging.error("Error: 'parentReference' does not exist in the API response.")
+            error_message = "Erreur : 'parentReference' n'existe pas dans la réponse de l'API."
+            self.logger.log_error(error_message)
             self.error_logs["Data Format Error"].append(f"Group ID: {group_id}, Channel ID: {channel_id}")
             drive_id = None
             parent_item_id = None
@@ -197,17 +182,16 @@ class ModelGraphTransfer:
                     self.logger.log_error(f"Échec de l'upload du fichier '{file_name}'.")
                 return file_name, status
 
-            # Barre de progression avec Rich
+            # Parcourir les fichiers et dossiers du partage réseau et les télécharger dans le canal Teams
             with Progress(
                     TextColumn("[progress.description]{task.description}"),
                     BarColumn(),
                     TextColumn("[progress.percentage]{task.percentage:>3.1f}%"),
-                    TimeRemainingColumn(),
-                    TextColumn("{task.completed}/{task.total} fichiers"),
+                    TextColumn("{task.completed}/{task.total} files"),
+                    TextColumn("[progress.files]{task.fields[filename]}")
             ) as progress:
-                task = progress.add_task("[cyan]Transfert des fichiers...", total=total_files)
+                task = progress.add_task("[green]Uploading files...", total=total_files, filename="")
 
-                # Parcourir les fichiers et dossiers du partage réseau et les télécharger dans le canal Teams
                 with ThreadPoolExecutor() as executor:
                     futures = []
                     for root, dirs, files in os.walk(depot_data_directory_path):
@@ -226,20 +210,20 @@ class ModelGraphTransfer:
                                         f"https://graph.microsoft.com/v1.0/sites/{site_id}/drive/items/{current_parent_item_id}/children",
                                         headers=self.headers, proxies=self.proxies
                                     ).json()['value'] if item['name'] == folder)
-                                self.logger.log_info(f"Dossier '{folder}' traité.")
+                                progress.update(task, filename=f"Folder: {folder}")
 
                         # Télécharger les fichiers dans le canal Teams avec une barre de progression
                         for file_name in files:
                             file_path = os.path.join(root, file_name)
                             futures.append(executor.submit(process_file, file_path, site_id, current_parent_item_id))
-                            progress.update(task, advance=1, description=f"Traitement de {file_name}")
+                            progress.update(task, filename=f"File: {file_name}")
 
                     completed_files = 0
-                    for future in as_completed(futures):
+                    for future in concurrent.futures.as_completed(futures):
                         file_name, status = future.result()
                         if status != "exists":
                             completed_files += 1
-                        progress.update(task, advance=1, description=f"Traitement de {file_name}")
+                        progress.update(task, advance=1, filename=f"File: {file_name}")
 
                     self.logger.log_info(f"Total des fichiers à copier : {total_files}")
                     self.logger.log_info(f"Fichiers copiés : {completed_files}")
